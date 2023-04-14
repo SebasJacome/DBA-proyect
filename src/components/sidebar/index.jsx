634fc7e4db829fb99da0f520b0a81ff8dbe5a3f0
SebasJacome <0237105@up.mx>
2023-04-14 17:51:49 -0600

    Agregado fondo, sidebar casi terminado

--- src/components/sidebar/index.jsx
+++ src/components/sidebar/index.jsx
@@ -1,25 +1,46 @@
 import React from "react"
 import './sidebar.scss'
 import josemi from '../../assets/josemi.jpeg'
+import world_ico from '../../assets/world-svgrepo-com.svg'
 
 const Sidebar = () =>{
     return (
         <div className="Sidebar">
-            <h1>Sidebar</h1>
-            <ul>
-                <li>Dashboard</li>
-                <li>Projects</li>
-                <li>Task List</li>
-                <li>Services</li>
-                <li>Notifications</li>
-                <li>Chat</li>
-            </ul>
+            <div className="sidebar-header">
+                <img src={world_ico}/>
+                <h1>WORLD</h1>   
+            </div>
+            <div>
+                <ul>
+                    <li className="sidebar-content-list">
+                        <img src=""/>
+                        Dashboard
+                    </li>
+                    <li className="sidebar-content-list">
+                        <img src=""/>
+                        Languages
+                    </li>
+                    <li className="sidebar-content-list">
+                        <img src=""/>
+                        Population
+                    </li>
+                    <li className="sidebar-content-list">
+                        <img src=""/>
+                        Dashboard
+                    </li>
+                    <li className="sidebar-content-list">
+                        <img src=""/>
+                        Dashboard
+                    </li>
+                    
+                </ul>
+            </div>
             <div className="user">
                 <div>
                     <img src={josemi}/>
                 </div>
-                <p>Josemi Sus</p>
-                <p>pepekuri@gmail.com</p>
+                <p className="user-name">Josemi Sus</p>
+                <p className="user-email">pepekuri@gmail.com</p>
             </div>
         </div>
     )
