0e68d10b8a33ff4ef18759fcba0749006ac0a135
SebasJacome <0237105@up.mx>
2023-04-15 20:31:14 -0600

    Modificando algunos errores de importación

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import Sidebar from './components/sidebar'
+import Sidebar from './components/Sidebar/Sidebar'
 
 import Dashboard from './layouts/Dashboard'
 import Prueba from './layouts/Prueba'
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () =>{
     return(
         <>
             <div className="graph">
-                <ResponsiveContainer height={"90%"}>
+                <ResponsiveContainer height={"99%"}>
                     <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -21,7 +21,7 @@ const Dashboard = () =>{
             </div>
 
             <div className="graph">
-                <ResponsiveContainer height={"90%"}>
+                <ResponsiveContainer height={"99%"}>
                     <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
@@ -33,7 +33,7 @@ const Dashboard = () =>{
             </div>
 
             <div className="graph">
-                <ResponsiveContainer height={"90%"}>
+                <ResponsiveContainer height={"99%"}>
                     <LineChart data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
                         <Line type="monotone" dataKey="uv" stroke="#8884d8" />
                         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
