eb0de6deac28b3ea909d08c033d529a1d8231d52
SebasJacome <0237105@up.mx>
2023-04-15 20:31:36 -0600

    Renombrando un archivo

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,39 @@
+import React from "react"
+import './Sidebar.scss'
+import josemi from '../../assets/josemi.jpeg'
+import world_ico from '../../assets/world-svgrepo-com.svg'
+
+const Sidebar = ({setTab}) =>{
+    const handleClick = (event) =>{
+        const tabText = event.target.innerText
+        setTab(tabText.toLowerCase())
+    }
+    return (
+        <div className="Sidebar">
+            <div className="sidebar-header">
+                <img src={world_ico}/>
+                <h1>PUTO CHINO</h1>   
+            </div>
+            <div>
+                <ul>
+                    <li className="sidebar-content-list" onClick={handleClick}>
+                        <img src=""/>
+                        Dashboard
+                    </li>
+                    <li className="sidebar-content-list" onClick={handleClick}>
+                        <img src=""/>
+                        Prueba
+                    </li>
+                </ul>
+            </div>
+            <div className="user">
+                <div>
+                    <img src={josemi}/>
+                </div>
+                <p className="user-name">Josemi Sus</p>
+                <p className="user-email">pepekuri@gmail.com</p>
+            </div>
+        </div>
+    )
+}
+export default Sidebar
